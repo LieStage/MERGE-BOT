70a5ae5f8adeca93c083b174ba60938449acf8a9
LieStage <118051769+LieStage@users.noreply.github.com>
2022-12-01 00:43:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,7 +44,7 @@ from __init__ import (
     replyDB,
 )
 from config import Config
-from Config import PORT
+from info import PORT
 from helpers import database
 from helpers.utils import UserSettings, get_readable_file_size, get_readable_time
 from plugins import web_server
